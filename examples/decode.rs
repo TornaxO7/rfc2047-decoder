2bdae7f0e3789049c1a7122b6fc9ae7bd832edd8
TornaxO7 <tornax07@gmail.com>
2022-06-07 16:55:59 +0200

    adjusted test

--- examples/decode.rs
+++ examples/decode.rs
@@ -1,13 +1,11 @@
 use rfc2047_decoder;
 
-fn main() -> rfc2047_decoder::Result<()> {
+fn main() {
     let encoded_str = "=?UTF-8?Q?str?=";
     let decoded_str = "str";
 
     assert_eq!(
-        rfc2047_decoder::decode(encoded_str.as_bytes())?,
+        rfc2047_decoder::decode(encoded_str.as_bytes()).unwrap(),
         decoded_str
     );
-
-    Ok(())
 }

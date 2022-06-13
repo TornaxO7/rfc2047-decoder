856c69bec1c28d21b3ba30684ccbddc0bb854597
TornaxO7 <tornax07@gmail.com>
2022-06-13 09:59:09 +0200

    trying to implement the parser

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,19 +1,27 @@
-use chumsky::{Parser, prelude::Simple};
+use chumsky::{prelude::Simple, Parser};
 
 const QUESTION_MARK: char = '?';
 const EQUAL_SYMBOL: char = '=';
 const SPACE: char = ' ';
 const ESPECIAL: &'static str = "{}<>@,@:/[]?.=";
 
+pub type Token = Vec<u8>;
+pub type EncodedText = Vec<u8>;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Expr {
-    Token(String),
-    EncodedText(String),
+    Charset(Token),
+    Encoding(Token),
+    EncodedText(EncodedText),
+    EncodedWord {
+        charset: Box<Expr>,
+        encoding: Box<Expr>,
+        encoded_text: Box<Expr>,
+    },
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct EncodedWord {
-}
+pub struct EncodedWord {}
 
 impl EncodedWord {
     pub const PREFIX: String = format!("{}{}", EQUAL_SYMBOL, QUESTION_MARK);
@@ -21,7 +29,6 @@ impl EncodedWord {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
-
 
 #[derive(thiserror::Error, Debug, Clone)]
 pub enum Error {
@@ -37,28 +44,43 @@ pub fn run(encoded_bytes: &[u8]) -> Result<EncodedWord> {
     Err(Error::ParseCharsetError)
 }
 
-fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
+fn parser() -> impl Parser<u8, Expr, Error = Simple<u8>> {
     use chumsky::prelude::*;
 
-    let token = filter(|c: &char| *c != SPACE
-                       && !c.is_ascii_control()
-                       && !is_especial(*c))
-        .repeated().at_least(1)
-        .collect::<String>()
-        .map(|token| Expr::Token(token.to_string()));
+    let token =
+        filter(|&c: &u8| c != (SPACE as u8) && !c.is_ascii_control() && !is_especial(c as char));
 
-    let encoded_text = filter(|c: &char| *c != QUESTION_MARK && *c != SPACE)
-        .repeated().at_least(1)
-        .collect::<String>()
-        .map(|encoded_text| Expr::EncodedText(encoded_text.to_string()));
+    let charset = token
+        .repeated()
+        .at_least(1)
+        .at_most(75)
+        .collect::<Vec<u8>>()
+        .map(|charset| Expr::Charset(charset));
+
+    let encoding = token
+        .repeated()
+        .at_least(1)
+        .collect::<Vec<u8>>()
+        .map(|encoding| Expr::Encoding(encoding));
+
+    let encoded_text = filter(|&c: &u8| c != (QUESTION_MARK as u8) && c != (SPACE as u8))
+        .repeated()
+        .at_least(1)
+        .collect::<Vec<u8>>()
+        .map(|encoded_text| Expr::EncodedText(encoded_text));
 
     let encoded_word = just(EncodedWord::PREFIX)
-        .then(token)
-        .then(just(QUESTION_MARK))
-        .then(token)
-        .then(just(QUESTION_MARK))
+        .ignore_then(charset)
+        .then_ignore(just(QUESTION_MARK))
+        .then(encoding)
+        .then_ignore(just(QUESTION_MARK))
         .then(encoded_text)
-        .then(just(EncodedWord::SUFFIX));
+        .then_ignore(just(EncodedWord::SUFFIX))
+        .map(|((charset, encoding), encoded_word)| Expr::EncodedWord {
+            charset: Box::new(charset),
+            encoding: Box::new(encoding),
+            encoded_text: Box::new(encoded_word),
+        });
 
     encoded_word
 }
